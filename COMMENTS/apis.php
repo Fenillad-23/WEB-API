615756221fe6eec8819ccb79f1a5b467ebcb49e1
fenil lad <fenillad087@gmail.com>
2024-03-10 21:35:32 -0400

    implemented update method for comments

--- COMMENTS/apis.php
+++ COMMENTS/apis.php
@@ -105,3 +105,81 @@ function addComment()
         }
     }
 }
+function updateComment(){
+    global $con;
+    parse_str(file_get_contents("php://input"), $_PUT);
+    if(!$con){
+        $data = ['status' => 500,'message'=> 'Database connection error'];
+        header("HTTP/1.0 500 Internal Server Error");
+        echo json_encode($data);
+        exit();
+    }else{
+        if(isset($_PUT['cmtId']) && !empty($_PUT['cmtId'])){
+            $cmtId= intval($_PUT["cmtId"]);
+            $rating = htmlspecialchars($_PUT['rating']);
+            $images = htmlspecialchars($_PUT['images']);
+            $text = htmlspecialchars($_PUT['text']);
+            
+          
+            $query = $con->prepare('UPDATE comments SET rating=?, images=?,text=? where cmtId=?');
+            if($query){
+                $query->bind_param('sssi', $rating,$images,$text,$cmtId);
+                if($query->execute()){
+                    $data = array('status' => '200', 'message' => 'comment info successfully updated');
+                    echo json_encode($data);
+                }else {
+                    http_response_code(500);
+                    $response = array('status' => 'error', 'message' => 'Problem while updating comment: ' . $con->error);
+                    echo json_encode($response);
+                }
+                $query->close();
+            }
+            $con->close();
+        }else{
+            http_response_code(400);
+            echo json_encode(array('error' => 'Please provide comment id to update record'));
+        }
+    }
+
+}
+function deleteComment()
+{
+    global $con;
+    parse_str(file_get_contents("php://input"), $_DELETE);
+    
+    if (!$con) {
+        $data = [
+            'status' => 500,
+            'message' => 'Database connection error',
+        ];
+        header("HTTP/1.0 500 Internal Server Error");
+        echo json_encode($data);
+        exit();
+    } else {
+        if (isset($_DELETE["cmtId"]) && !empty($_DELETE["cmtId"])) {
+            $cmtId = $_DELETE["cmtId"];
+           
+            $query = $con->prepare("DELETE FROM comments WHERE cmtId = ?");
+            if ($query) {
+                $query->bind_param("i", $cmtId);
+                if ($query->execute()) {
+                    $data = array('status' => 200, 'message' => 'Comment removed successfully');
+                    echo json_encode($data);
+                } else {
+                    http_response_code(500);
+                    $response = array('status' => 'error', 'message' => 'Problem while removing comment: ' . $con->error);
+                    echo json_encode($response);
+                }
+                $query->close();
+            } else {
+                http_response_code(500);
+                $response = array('status' => 'error', 'message' => 'Problem preparing query: ' . $con->error);
+                echo json_encode($response);
+            }
+            $con->close();
+        } else {
+            http_response_code(400);
+            echo json_encode(array('error' => 'Please provide comment id'));
+        }
+    }
+}
